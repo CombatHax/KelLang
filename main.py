c4084ae662df4e7aa7d36db1121e1a826dac711d
CombatHax <92267016+CombatHax@users.noreply.github.com>
2022-06-06 16:27:08 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,23 +1,19 @@
 import sys
 import re
 
+variables = {}
+
 def main():
     if len(sys.argv) < 2:
         return -1
-    variables = {}
     f = open(sys.argv[1], 'r')
     lines = f.readlines()
     for line in lines:
+        line = vars_to_value(line)
         if re.match("SAY ", line.upper()):
             string = line[4:]
             if string[-1] == '\n':
                 string = string[:-1]
-            var_finder = r"var\([a-zA-Z]+\)"
-            var = re.search(var_finder, string)
-            while var:
-                str_var = string[var.start():var.end()]
-                string = re.sub(re.escape(str_var), variables[str_var[4:-1]], string)
-                var = re.search(var_finder, string)
             print(string)
         elif re.match("SET ", line.upper()):
             name_end = line[4:].index(' ') + 4
@@ -28,10 +24,25 @@ def main():
                 value = line[name_end + 1:]
             if value[-1] == '\n':
                 value = value[:-1]
-            try:
-                value = float(value)
-            except:
+            nice = r"[^0-9\+\-\/\* ]"
+            if re.search(nice, value):
                 pass
+            else:
+                value = eval(value)
             variables[name] = value
+def vars_to_value(string):
+    try:
+        var_finder = r"var\([a-zA-Z]+\)"
+        var = re.search(var_finder, string)
+        finished = string
+        while var:
+            str_var = finished[var.start():var.end()]
+            finished = re.sub(re.escape(str_var), str(variables[str_var[4:-1]]), finished)
+            var = re.search(var_finder, finished)
+    except KeyError:
+        ty, val, trace = sys.exc_info()
+        print(f"Unkown Variable: {val}")
+        exit()
+    return finished
 if __name__ == "__main__":
     main()
